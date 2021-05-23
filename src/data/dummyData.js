102475d1592bb28af694250994431b2feb624734
Paul Dredge <dredgos@googlemail.com>
2021-05-23 17:15:55 +0100

    added dummy API data for development

---
+++ src/data/dummyData.js
@@ -0,0 +1,67 @@
+export const allRecipes = {
+    "data": [
+        {
+            "recipe_id": 1,
+            "recipe_name": "Fish Finger Sandwich",
+            "difficulty": "Easy",
+            "time": 25,
+            "category": "Fish",
+            "ingredients": {
+                "Fish Fingers": "4 Fingers",
+                "Bread": "2 Slices"
+            },
+            "method": "Cook fish fingers as per instructions, butter bread, construct sandwich.",
+            "updated_at": "14 minutes ago"
+        }, 
+        {
+            "recipe_id": 1,
+            "recipe_name": "Fish Finger Sandwich",
+            "difficulty": "Easy",
+            "time": 25,
+            "category": "Fish",
+            "ingredients": {
+                "Fish Fingers": "4 Fingers",
+                "Bread": "2 Slices"
+            },
+            "method": "Cook fish fingers as per instructions, butter bread, construct sandwich.",
+            "updated_at": "14 minutes ago"
+        }, 
+        {
+            "recipe_id": 1,
+            "recipe_name": "Fish Finger Sandwich",
+            "difficulty": "Easy",
+            "time": 25,
+            "category": "Fish",
+            "ingredients": {
+                "Fish Fingers": "4 Fingers",
+                "Bread": "2 Slices"
+            },
+            "method": "Cook fish fingers as per instructions, butter bread, construct sandwich.",
+            "updated_at": "14 minutes ago"
+        }
+    ]
+}
+
+export const oneRecipe = {
+    "recipe_id": 1,
+    "recipe_name": "Fish Finger Sandwich",
+    "difficulty": "Easy",
+    "time": 25,
+    "category": "Fish",
+    "ingredients": {
+        "Fish Fingers": "4 Fingers",
+        "Bread": "2 Slices"
+    },
+    "method": "Cook fish fingers as per instructions, butter bread, construct sandwich.",
+    "updated_at": "14 minutes ago"
+}
+
+export const postRecipe = {
+    "recipe_name": "Fish Finger Sandwich",
+    "difficulty": "Easy",
+    "time": 15,
+    "category": "Fish",
+    "method": "Cook fish fingers as per instructions, butter bread, construct sandwich.",
+    "ingredients": ["Fish Fingers", "Bread"],
+    "amounts": ["4 Fingers", "2 Slices"]
+}
