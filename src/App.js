cbeacc3b830baf212f745a68b3ea5298fd896d5b
Paul Dredge <lardarse7@hotmail.com>
2021-05-28 17:30:41 +0100

    Installed SASS, created stylesheets and added folders

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import './styles/main.scss'
+
+import Home from './components/homepage/Home'
+
 const App = () => {
 
     return (
-    <>
-    
-    </>
+      <>
+        <Home />
+      </>
     )   
 }
 export default App;
