0cdb8ad6888e1107b53b6dcef7c80f8b41cb0072
Paul Dredge <dredgos@googlemail.com>
2021-09-30 17:30:36 +0100

    Added react redux, initialised a store and created basic reducer

---
+++ src/data/Initial.js
@@ -0,0 +1,5 @@
+const initial = {
+    meals: []
+}
+
+export default initial;
---
+++ src/data/Reducer.js
@@ -0,0 +1,10 @@
+import initial from './Initial';
+
+const reducer = (state, action) => {
+    switch(action.type) {
+
+        default: return initial;
+    }
+}
+
+export default reducer;
